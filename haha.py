1812cc1bc3e2334049bfb11e57e5b082964ed54f
Zekun Li <1500017841@pku.edu.cn>
2018-06-07 22:13:01 +0800

    Update haha.py

--- haha.py
+++ haha.py
@@ -9,8 +9,9 @@
 #
 # @return
 def get_ftp_file(dataset, date):
+    date = str(date)
     return "/index/{date}_{date}d_{dataset}.zip".format(
-        date = date
+        date = date,
         dataset = dataset
     )
 
@@ -22,6 +23,7 @@ def get_ftp_file(dataset, date):
 #
 # @return
 def get_local_file(dataset, date):
+    date = str(date)
     return "~/data/stock/msci/{dataset}/{yyyy}/".format(
         dataset = dataset,
         yyyy=date[0:4],
@@ -41,232 +43,38 @@ def get_index_weight_file(dataset, date):
 def download_ftp_file(dataset, date):
         ftp_file = get_ftp_file(dataset, date)
         local_path = get_local_file(dataset, date)
-        local_zip_file = local_file + dataset + "_" + date + ".zip"
+        local_zip_file = local_path + dataset + "_" + date + ".zip"
         from ftplib import FTP
         import os
         import zipfile
-        FTP_SERVER = ""
-        USER = ""
-        PWD = ""
+        # FTP_SERVER = ""
+        # USER = ""
+        # PWD = ""
 
-        try:
-            if os.path.exists(local_zip_file):
-                print("--file exists!--")
-            else:
-                os.mkdirs(local__zip_file)
-            return True
-        except:
+
+        if os.path.exists(local_zip_file):
+            print("--file exists!--")
             return False
 
-        ftp = FTP()
-        ftp.connect(FTP_SERVER, 21)
-        ftp.login(USER, PWD)
-
-        ftp = ftpconnect()
-        li = ftp.nlst(ftp_file)
-
-        fp = open(local_zip_file, "wb+")
-        bufsize = 1024
-        ftp.retrbinary('RETR' + li[0], fp.write, bufsize)
-        fp.flush()
-
-        fp.close()
-        ftp.quit()
+        # 
+        # ftp = FTP()
+        # ftp.connect(FTP_SERVER, 21)
+        # ftp.login(USER, PWD)
+        # 
+        # ftp = ftpconnect()
+        # li = ftp.nlst(ftp_file)
+        # 
+        # fp = open(local_zip_file, "wb+")
+        # bufsize = 1024
+        # ftp.retrbinary('RETR' + li[0], fp.write, bufsize)
+        # fp.flush()
+        # 
+        # fp.close()
+        # ftp.quit()
 
         azip = zipfile.ZipFile(local_zip_file)
         azip.extractall()
 
-    ───────────────────────────────────────────────────────────────────────────────────────────────
-
-    Session
-    stopped
-    - Press <
-    return > to
-    exit
-    tab
-    - Press
-    R
-    to
-    restart
-    session
-    - Press
-    S
-    to
-    save
-    terminal
-    output
-    to
-    file
-
-
-Network
-error: Connection
-timed
-out
-
-───────────────────────────────────────────────────────────────────────────────────────────────
-
-Session
-stopped
-- Press <
-return > to
-exit
-tab
-- Press
-R
-to
-restart
-session
-- Press
-S
-to
-save
-terminal
-output
-to
-file
-
-Network
-error: Connection
-timed
-out
-login as: lizekun
-Access
-denied
-lizekun @ 10.24
-.10
-.235
-'s password:
-┌────────────────────────────────────────────────────────────────────┐
-│                        • MobaXterm
-10.4 •                          │
-│            (SSH client, X-server and networking tools)             │
-│                                                                    │
-│ ➤ SSH
-session
-to
-lizekun @ 10.24
-.10
-.235                              │
-│   • SSH
-compression: ✔                                            │
-│   • SSH - browser: ✔                                            │
-│   • X11 - forwarding: ✔  (remote display is forwarded through SSH) │
-│   • DISPLAY: ✔  (automatically set on remote server)      │
-│                                                                    │
-│ ➤ For
-more
-info, ctrl + click
-on
-help or visit
-our
-website           │
-└────────────────────────────────────────────────────────────────────┘
-
-Welcome
-to
-Ubuntu
-16.04
-.3
-LTS(GNU / Linux
-4.13
-.0 - 38 - generic
-x86_64)
-
-*Documentation: https: // help.ubuntu.com
-*Management: https: // landscape.canonical.com
-*Support: https: // ubuntu.com / advantage
-
-188
-packages
-can
-be
-updated.
-8
-updates
-are
-security
-updates.
-
-** *System
-restart
-required ** *
-Last
-login: Thu
-Jun
-7
-10: 03:27
-2018
-from
-
-10.24
-.10
-.61
-➜  ~ fls
-zsh: command
-not found: fls
-➜  ~ ls
-examples.desktop
-room
-➜  ~ cd
-room
-➜  room
-ls
-alphaman.py
-edstats.py
-make
-pnl
-src
-test_auto.py
-auto.py
-filter.py
-Makefile
-py
-stats.py
-update_room.py
-csa.py
-lib
-one_by_one.py
-room_config.py
-summ.py
-xml
-➜  room. / update_room.py
-cp / opt / room / *.-rf
-room is updated.enjoy
-it
-➜  room
-ls
-alphaman.py
-edstats.py
-make
-pnl
-src
-test_auto.py
-auto.py
-filter.py
-Makefile
-py
-stats.py
-update_room.py
-csa.py
-lib
-one_by_one.py
-room_config.py
-summ.py
-xml
-➜  room
-vim
-py / util / mscic
-➜  room. / update_room.py
-cp / opt / room / *.-rf
-room is updated.enjoy
-it
-➜  room
-vim
-py / util / msci_index.py
-
-azip = zipfile.ZipFile(local_zip_file)
-azip.extractall()
 
 
 # try to download ftp_file, save to local_file
@@ -338,4 +146,3 @@ if __name__ == "__main__":
         raw_file = get_local_raw_file(options.dataset, options.date)
         index_weight_file = get_index_weight_file(options.dateset, options.date)
         parse_raw_file_and_save(raw_file, index_weight_file)
--- VISUAL LINE --                                                                                       145,9         Bot
